c50982e6f22ab7557adff22e85ade7adaad8902f
Peter LaValle <peter.lavalle@gmail.com>
2024-02-22 18:10:09 +0000

    Update al.js

--- al.js
+++ al.js
@@ -1,5 +1,5 @@
 alert('hello friend 3');
-var v =  window.getElementsByTagName('*');
+var v =  window.getElementsByTagName('video');
 alert('letse sse');
 alert('there are '+len(v))
 for (var video of v)
