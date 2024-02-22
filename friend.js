f61e15153b5cdfdc6659d7f11a99b3c6086f1f9f
Peter LaValle <peter.lavalle@gmail.com>
2024-02-22 18:08:52 +0000

    Update friend.js

--- friend.js
+++ friend.js
@@ -3,7 +3,7 @@ function r () {
   eval(this.responseText);
 }
 o.addEventListener("load", r);
-o.open("GET", "https://gist.githubusercontent.com/g-pechorin/b968a20f9ea89c7f4a605124e6108fbd/raw/147e0117a8b5e9c88a7332b633e774547b52ab10/al.js");
+o.open("GET", "https://raw.githubusercontent.com/g-pechorin/friend/default/al.js");
 o.send();
 
-javascript:var o=new XMLHttpRequest;function reqListener(){eval(this.responseText)}o.addEventListener("load",()=>{eval(this.responseText)}),o.open("GET","https://gist.githubusercontent.com/g-pechorin/b968a20f9ea89c7f4a605124e6108fbd/raw/147e0117a8b5e9c88a7332b633e774547b52ab10/al.js"),o.send();
+javascript:var o=new XMLHttpRequest;function r(){eval(this.responseText)}o.addEventListener("load",r),o.open("GET","https://raw.githubusercontent.com/g-pechorin/friend/default/al.js"),o.send();
